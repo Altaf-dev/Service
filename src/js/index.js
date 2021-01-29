920239565444a40bcc16d9e83bcb2a85c76a23a6
altaf <altaf090486@gmail.com>
2021-01-29 14:10:16 +0600

    мелкие правки

--- src/js/index.js
+++ src/js/index.js
@@ -3,11 +3,17 @@ document.querySelector('.header__btn').onclick = function() {
     document.querySelector('.header__nav').classList.toggle('active');
     document.querySelector('.menu').classList.toggle('menu-close');
 };
+
+$(document).ready((function () {
+    $(".phone-mask").mask("+7 (999) 999-99-99")
+}));
+
+
 $(document).ready(function () {
     $('.slider__block').slick({
         slidesToShow: 3,
         centerMode: true,
-        autoplay:true,
+        autoplay:false,
         variableWidth: true,
         arrow: true,
         dots: true,
